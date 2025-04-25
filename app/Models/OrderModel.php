6d0786eab179a727ed80048f15bc97f3d97e2157
Xuanlapp <xuanlap250203@gmail.com>
2025-04-25 11:57:07 +0700

    done

--- app/Models/OrderModel.php
+++ app/Models/OrderModel.php
@@ -1,42 +1,42 @@
 <?php
 
 
-   namespace Models;
+namespace App\Models;
 
-   use CodeIgniter\Model;
+use CodeIgniter\Model;
 
-   class OrderModel extends Model
-   {
-      protected $table = 'orders';
-      protected $primaryKey = 'id';
-      protected $useAutoIncrement = true;
-      protected $returnType = 'array';
-      protected $useSoftDeletes = false;
-      protected $protectFields = true;
-      protected $allowedFields = [
-         'customer_name',
-         'amount',
-         'payment_type',
-         'payment_status',
-         'payment_details',
-         'transaction_id',
-         'response_message',
-         'created_at',
-         'updated_at'
-      ];
+class OrderModel extends Model
+{
+    protected $table = 'orders';
+    protected $primaryKey = 'id';
+    protected $useAutoIncrement = true;
+    protected $returnType = 'array';
+    protected $useSoftDeletes = false;
+    protected $protectFields = true;
+    protected $allowedFields = [
+        'customer_name',
+        'amount',
+        'payment_type',
+        'payment_status',
+        'payment_details',
+        'transaction_id',
+        'response_message',
+        'created_at',
+        'updated_at'
+    ];
 
-      // Dates
-      protected $useTimestamps = true;
-      protected $dateFormat = 'datetime';
-      protected $createdField = 'created_at';
-      protected $updatedField = 'updated_at';
+    // Dates
+    protected $useTimestamps = true;
+    protected $dateFormat = 'datetime';
+    protected $createdField = 'created_at';
+    protected $updatedField = 'updated_at';
 
-      // Validation
-      protected $validationRules = [
-         'customer_name' => 'required|min_length[3]|max_length[255]',
-         'amount' => 'required|numeric|greater_than[0]',
-         'payment_type' => 'required|in_list[momo,stripe,paypal]',
-      ];
-      protected $validationMessages = [];
-      protected $skipValidation = false;
-   }
+    // Validation
+    protected $validationRules = [
+        'customer_name' => 'required|min_length[3]|max_length[255]',
+        'amount' => 'required|numeric|greater_than[0]',
+        'payment_type' => 'required|in_list[momo,stripe,paypal]',
+    ];
+    protected $validationMessages = [];
+    protected $skipValidation = false;
+}
--- app/Services/PaymentGatewayInterface.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace App\Services;
-
-interface PaymentGatewayInterface
-{
-    public function processPayment(array $orderData): array;
-}
